c71ccda4df808774b9a754400b2cdb1ed8f1236f
Pierre-Francois Loos <pierrefrancois.loos@gmail.com>
2020-02-12 21:44:35 +0100

    experimenting with QP_roots

--- src/QuAcK/QP_roots.f90
+++ src/QuAcK/QP_roots.f90
@@ -1,4 +1,4 @@
-subroutine QP_roots(nBas,nC,nO,nV,nR,nS,eta,eHF,Omega,rho)
+subroutine QP_roots(nBas,nC,nO,nV,nR,nS,eta,eHF,Omega,rho,eGW)
 
 ! Compute all the roots of the QP equation for each orbital
 
@@ -21,10 +21,9 @@ subroutine QP_roots(nBas,nC,nO,nV,nR,nS,eta,eHF,Omega,rho)
 ! Local variables
 
   integer                       :: i,j,a,b,x,jb,g
-  integer                       :: nRoot
-  integer,parameter             :: nGrid = 10000
-  double precision,parameter    :: wmin = -10d0
-  double precision,parameter    :: wmax = +10d0
+  integer,parameter             :: nGrid = 100000
+  double precision,parameter    :: wmin = -50d0
+  double precision,parameter    :: wmax = +50d0
   double precision              :: dw
   double precision              :: eps
   double precision              :: left_sign,right_sign
@@ -37,9 +36,17 @@ subroutine QP_roots(nBas,nC,nO,nV,nR,nS,eta,eHF,Omega,rho)
   integer,parameter             :: maxIt = 64
   double precision,parameter    :: thresh = 1d-6
   double precision,external     :: SigmaC,dSigmaC
-  double precision              :: f,df
-  double precision              :: s,ds
+  double precision              :: sig,dsig
+  double precision              :: sat,dsat
   double precision              :: om
+  integer                       :: nRoot
+  double precision              :: Z
+  double precision              :: Ztot
+  double precision              :: QP
+
+! Output variables
+
+  double precision,intent(out)  :: eGW(nBas)
 
 ! Construct grid
 
@@ -101,6 +108,8 @@ subroutine QP_roots(nBas,nC,nO,nV,nR,nS,eta,eHF,Omega,rho)
 ! Find the zeros of the QP equation
 
     nRoot = 0
+    QP    = 0d0
+    Ztot  = 0d0
 
     write(*,*) '-----------------'
     write(*,'(A10,I3)') 'Orbital ',x
@@ -124,31 +133,48 @@ subroutine QP_roots(nBas,nC,nO,nV,nR,nS,eta,eHF,Omega,rho)
 
           om = w(g-1)
           nIt = 0
-          f = 1d0
+          sat = 1d0
     
-          do while (abs(f) > thresh .and. nIt < maxIt)
+          do while (abs(sat) > thresh .and. nIt < maxIt)
     
             nIt = nIt + 1
 
-            s  =  SigmaC(x,om,eta,nBas,nC,nO,nV,nR,nS,eHF,Omega,rho)
-            ds = dSigmaC(x,om,eta,nBas,nC,nO,nV,nR,nS,eHF,Omega,rho)
-            f  = om - eHF(x) - s
-            df = 1d0 - ds
+            sig  =  SigmaC(x,om,eta,nBas,nC,nO,nV,nR,nS,eHF,Omega,rho)
+            dsig = dSigmaC(x,om,eta,nBas,nC,nO,nV,nR,nS,eHF,Omega,rho)
+            sat  = om - eHF(x) - sig
+            dsat = 1d0 - dsig
+            Z    = 1d0/dsat
     
-            write(*,'(A3,I3,A1,1X,4F15.9)') 'It.',nIt,':',om,f,s,1d0/(1d0-ds)
+            write(*,'(A3,I3,A1,1X,4F15.9)') 'It.',nIt,':',om,sat,sig,Z
     
-            om = om - f/df
+            om = om - sat/dsat
     
           end do
+ 
+          if(nIt == maxIt) then 
 
+            write(*,*) 'Newton root search has not converged!'
+
+          else 
+
+            Ztot = Ztot + Z
+            QP   = QP + Z*om
+
+          end if
+          
       end if
 
       left_sign = right_sign
       left_QP   = right_QP
    
     end do
+
+    eGW(x) = QP/Ztot
+
     write(*,*)
     write(*,'(A32,I3,A1,I3)') 'Number of roots for orbital ',x,':',nRoot
+    write(*,'(A32,F16.10)')   'Total weight                ',Ztot
+    write(*,'(A32,F16.10)')   'Quasiparticle energy (eV)   ',eGW(x)*HaToeV
     write(*,*)
 
   end do
