5a80b38685d45996c9a1b43404fbee387594f768
Pierre-Francois Loos <pierrefrancois.loos@gmail.com>
2020-04-02 09:53:36 +0200

    individual energies for HF

--- src/eDFT/fock_exchange_individual_energy.f90
+++ src/eDFT/fock_exchange_individual_energy.f90
@@ -13,7 +13,6 @@ subroutine fock_exchange_individual_energy(nBas,Pw,P,ERI,Ex)
 
 ! Local variables
 
-  integer                       :: mu,nu,la,si
   double precision,allocatable  :: Fx(:,:)
   double precision,external     :: trace_matrix
 
@@ -25,18 +24,8 @@ subroutine fock_exchange_individual_energy(nBas,Pw,P,ERI,Ex)
 
   allocate(Fx(nBas,nBas))
 
-  Fx(:,:) = 0d0
+  call fock_exchange_potential(nBas,P(:,:),ERI(:,:,:,:),Fx(:,:))
 
-  do si=1,nBas
-    do la=1,nBas
-      do nu=1,nBas
-        do mu=1,nBas
-          Fx(mu,nu) = Fx(mu,nu) + P(la,si)*ERI(mu,la,si,nu)
-        enddo
-      enddo
-    enddo
-  enddo
-
-  Ex = -0.25d0*trace_matrix(nBas,matmul(P(:,:),Fx(:,:))) 
+  Ex = 0.5d0*trace_matrix(nBas,matmul(P(:,:),Fx(:,:))) !&
 
 end subroutine fock_exchange_individual_energy
