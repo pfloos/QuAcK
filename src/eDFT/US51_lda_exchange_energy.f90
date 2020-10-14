bf7cc8dadad5686e60f4210edd2a300aaabb1969
Pierre-Francois Loos <pierrefrancois.loos@gmail.com>
2020-10-14 23:31:14 +0200

    clean up

--- src/eDFT/US51_lda_exchange_energy.f90
+++ src/eDFT/US51_lda_exchange_energy.f90
@@ -1,4 +1,4 @@
-subroutine US51_lda_exchange_energy(nGrid,weight,rho,ExLDA)
+subroutine US51_lda_exchange_energy(nGrid,weight,rho,Ex)
 
   use xc_f90_lib_m
 
@@ -15,40 +15,41 @@ subroutine US51_lda_exchange_energy(nGrid,weight,rho,ExLDA)
 
 ! Local variables
 
-  integer(8)                    :: nGri8 
   integer                       :: iG
-  double precision              :: r
-  double precision,allocatable  :: Ex(:)
-
-  TYPE(xc_f90_func_t) :: xc_func
-  TYPE(xc_f90_func_info_t) :: xc_info
-  integer :: func_id = 1
+  double precision              :: alpha,r,alphaw,a2,b2,c2,a1,b1,c1
 
 ! Output variables
 
-  double precision              :: ExLDA
+  double precision              :: Ex
 
-! Memory allocation
-  
-  nGri8 = int(nGrid,8)
-  print*,nGri8
-  allocate(Ex(nGrid))
+! Cxw2 parameters for He N->N+1
+!  a2 = 0.135068d0
+!  b2 = -0.00774769d0
+!  c2 = -0.0278205d0
 
-  call xc_f90_func_init(xc_func, func_id, XC_POLARIZED)
-  xc_info = xc_f90_func_get_info(xc_func)
-  call xc_f90_lda_exc(xc_func, nGri8, rho(1), Ex(1))
+! Cxw1 parameters for He N->N-1
+!  a1 = 0.420243d0
+!  b1 = 0.0700561d0
+!  c1 = -0.288301d0
 
-  ExLDA = 0d0
+! Cx coefficient for Slater LDA exchange
 
-! do iG=1,nGrid
+  alpha = -(3d0/2d0)*(3d0/(4d0*pi))**(1d0/3d0)
 
-! write(*,"(F8.6,1X,F9.6)") rho(iG), Ex(iG)
+!  alphaw = alpha*(1d0 - wEns(2)*(1d0 - wEns(2))*(a1 + b1*(wEns(2) - 0.5d0) + c1*(wEns(2) - 0.5d0)**2))
+! Compute LDA exchange energy
 
-!     ExLDA = ExLDA  + weight(iG)*Ex(iG)
+  Ex = 0d0
+  do iG=1,nGrid
 
-! enddo
+    r = max(0d0,rho(iG))
 
-  call xc_f90_func_end(xc_func)
+    if(r > threshold) then
 
+      Ex = Ex  + weight(iG)*alpha*r**(4d0/3d0)
+
+    endif
+
+  enddo
 
 end subroutine US51_lda_exchange_energy
