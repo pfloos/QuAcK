aab825a4a35d5b261c889dfd6e6f2b2245bbb6ab
Pierre-Francois Loos <loos@lcpqmc74.univ-tlse3.fr>
2023-11-14 14:37:17 +0100

    introduce default methods and options

--- test/run_test.sh
+++ test/run_test.sh
@@ -4,3 +4,5 @@ cp ./methods.test ../input/methods
 cp ./options.test ../input/options
 cd ..
 python3 PyDuck.py -x He -b 6-31g -m 1
+cp input/methods.default input/methods
+cp input/options.default input/options
