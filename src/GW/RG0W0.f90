87245a33e258e3af8a84d524e17669d1512a5489
pfloos <pierrefrancois.loos@gmail.com>
2024-01-18 11:53:20 +0100

    RGW+C

--- src/GW/RG0W0.f90
+++ src/GW/RG0W0.f90
@@ -163,7 +163,7 @@ subroutine RG0W0(dotest,doACFDT,exchange_kernel,doXBS,dophBSE,dophBSE2,TDA_W,TDA
 ! Dump results !
 !--------------!
 
-! call print_RG0W0(nBas,nO,eHF,ENuc,ERHF,SigC,Z,eGW,EcRPA,EcGM)
+  call print_RG0W0(nBas,nO,eHF,ENuc,ERHF,SigC,Z,eGW,EcRPA,EcGM)
 
 ! Perform BSE calculation
 
