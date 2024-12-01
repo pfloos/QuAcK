6cfe4a5dece390784ddcc1b6c2f70afe7bac7671
AbdAmmar <abd.ammar.phys@gmail.com>
2024-12-01 11:22:29 +0100

    OMP for phLR

--- src/LR/phLR_B.f90
+++ src/LR/phLR_B.f90
@@ -17,6 +17,8 @@ subroutine phLR_B(ispin,dRPA,nBas,nC,nO,nV,nR,nS,lambda,ERI,Bph)
   double precision              :: delta_dRPA
 
   integer                       :: i,j,a,b,ia,jb
+  integer                       :: nn,jb0
+  double precision              :: ct1,ct2
 
 ! Output variables
 
@@ -31,21 +33,44 @@ subroutine phLR_B(ispin,dRPA,nBas,nC,nO,nV,nR,nS,lambda,ERI,Bph)
 
   if(ispin == 1) then
 
-    ia = 0
-    do i=nC+1,nO
-      do a=nO+1,nBas-nR
-        ia = ia + 1
-        jb = 0
-        do j=nC+1,nO
-          do b=nO+1,nBas-nR
-            jb = jb + 1
- 
-            Bph(ia,jb) = 2d0*lambda*ERI(i,j,a,b) - (1d0 - delta_dRPA)*lambda*ERI(i,j,b,a)
- 
-          end do
-        end do
-      end do
-    end do
+    nn = nBas - nR - nO
+    ct1 = 2d0 * lambda
+    ct2 = - (1d0 - delta_dRPA) * lambda
+    !$OMP PARALLEL DEFAULT(NONE)            &
+    !$OMP PRIVATE (i, a, j, b, ia, jb0, jb) &
+    !$OMP SHARED (nC, nO, nR, nBas, nn, ct1, ct2, ERI, Bph)
+    !$OMP DO COLLAPSE(2)
+    do i = nC+1, nO
+      do a = nO+1, nBas-nR
+        ia = a - nO + (i - nC - 1) * nn
+
+        do j = nC+1, nO
+          jb0 = (j - nC - 1) * nn - nO
+
+          do b = nO+1, nBas-nR
+            jb = b + jb0
+
+            Bph(ia,jb) = ct1 * ERI(i,j,a,b) + ct2 * ERI(i,j,b,a)
+          enddo
+        enddo
+      enddo
+    enddo
+    !$OMP END DO
+    !$OMP END PARALLEL
+
+    !ia = 0
+    !do i=nC+1,nO
+    !  do a=nO+1,nBas-nR
+    !    ia = ia + 1
+    !    jb = 0
+    !    do j=nC+1,nO
+    !      do b=nO+1,nBas-nR
+    !        jb = jb + 1
+    !        Bph(ia,jb) = 2d0*lambda*ERI(i,j,a,b) - (1d0 - delta_dRPA)*lambda*ERI(i,j,b,a)
+    !      end do
+    !    end do
+    !  end do
+    !end do
 
   end if
 
@@ -53,21 +78,43 @@ subroutine phLR_B(ispin,dRPA,nBas,nC,nO,nV,nR,nS,lambda,ERI,Bph)
 
   if(ispin == 2) then
 
-    ia = 0
-    do i=nC+1,nO
-      do a=nO+1,nBas-nR
-        ia = ia + 1
-        jb = 0
-        do j=nC+1,nO
-          do b=nO+1,nBas-nR
-            jb = jb + 1
- 
-            Bph(ia,jb) = - (1d0 - delta_dRPA)*lambda*ERI(i,j,b,a)
- 
-          end do
-        end do
-      end do
-    end do
+    nn = nBas - nR - nO
+    ct2 = - (1d0 - delta_dRPA) * lambda
+    !$OMP PARALLEL DEFAULT(NONE)            &
+    !$OMP PRIVATE (i, a, j, b, ia, jb0, jb) &
+    !$OMP SHARED (nC, nO, nR, nBas, nn, ct2, ERI, Bph)
+    !$OMP DO COLLAPSE(2)
+    do i = nC+1, nO
+      do a = nO+1, nBas-nR
+        ia = a - nO + (i - nC - 1) * nn
+
+        do j = nC+1, nO
+          jb0 = (j - nC - 1) * nn - nO
+
+          do b = nO+1, nBas-nR
+            jb = b + jb0
+
+            Bph(ia,jb) = ct2 * ERI(i,j,b,a)
+          enddo
+        enddo
+      enddo
+    enddo
+    !$OMP END DO
+    !$OMP END PARALLEL
+
+!    ia = 0
+!    do i=nC+1,nO
+!      do a=nO+1,nBas-nR
+!        ia = ia + 1
+!        jb = 0
+!        do j=nC+1,nO
+!          do b=nO+1,nBas-nR
+!            jb = jb + 1
+!            Bph(ia,jb) = - (1d0 - delta_dRPA)*lambda*ERI(i,j,b,a)
+!          end do
+!        end do
+!      end do
+!    end do
 
   end if
 
