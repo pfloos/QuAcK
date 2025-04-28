54981296475ef72f28379a050712d87b8049405b
Mauricio Rodriguez-Mayorga <marm3.14@gmail.com>
2025-04-28 23:01:46 +0200

    Imporved master

--- src/AOtoMO/sigc_matrix_AO_basis.f90
+++ src/AOtoMO/sigc_matrix_AO_basis.f90
@@ -62,7 +62,6 @@ allocate(R(nOrb2,nOrb2))
 R(:,:)     = 0d0
 do iorb=1,nOrb
  R(:,:) = R(:,:) + matmul(U_QP(:,iorb:iorb),transpose(U_QP(:,iorb:iorb))) 
-
 enddo
 do iorb=1,nOrb2
   write(*,'(*(f10.5))') R(iorb,:)
