3d8ad6661cdd3a34902f83827a92bf437afd42ac
lburth <loris.burth@gmail.com>
2025-04-30 16:14:36 +0200

    rm unnecessary files

--- submission_scripts/run_test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cp ./methods.test ../input/methods
-cp ./options.test ../input/options
-basis=$2
-molecule=$1
-cp  ../cap_integrals/$basis ../int/CAP.dat
-cd ..
-python3.11 PyDuck.py -x $molecule -b $basis -c 0 -m 1 --use_local_basis
