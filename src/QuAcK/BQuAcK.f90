55f0c98aed05215397e505397e5eddec5e423570
Mauricio Rodriguez-Mayorga <marm3.14@gmail.com>
2025-06-28 15:33:34 +0200

    Incl. building Wp_AO(iw)

--- src/QuAcK/BQuAcK.f90
+++ src/QuAcK/BQuAcK.f90
@@ -72,6 +72,7 @@ subroutine BQuAcK(working_dir,dotest,doHFB,doqsGW,nNuc,nBas,nOrb,nC,nO,nV,ENuc,Z
   double precision              :: EcRPA,EcGM,trace,trace2,trace3
   double precision,allocatable  :: Chi0_ao_iw_v(:,:)
   double precision,allocatable  :: Chi_ao_iw_v(:,:)
+  double precision,allocatable  :: Wp_ao_iw(:,:)
   double precision,allocatable  :: eigval_eps(:)
   double precision,allocatable  :: eps(:,:)
   double precision,allocatable  :: epsm1(:,:)
@@ -183,6 +184,7 @@ subroutine BQuAcK(working_dir,dotest,doHFB,doqsGW,nNuc,nBas,nOrb,nC,nO,nV,ENuc,Z
 
      allocate(eps(nBas2,nBas2),epsm1(nBas2,nBas2),eigenv_eps(nBas2,nBas2),eigval_eps(nBas2))
      allocate(Chi0_ao_iw_v(nBas2,nBas2),Chi_ao_iw_v(nBas2,nBas2))
+     allocate(Wp_ao_iw(nBas2,nBas2))
 
      call iGtau2Chi0iw(nBas,nOrb,nC,nO,nV,cHFB,eHF,nfreqs,ntimes,wcoord,Chi0_ao_iw)
 
@@ -190,8 +192,9 @@ subroutine BQuAcK(working_dir,dotest,doHFB,doqsGW,nNuc,nBas,nOrb,nC,nO,nV,ENuc,Z
 
      write(*,*)     
      write(*,*)'***********************************'
-     write(*,*)'* Use Xo(i w) to build X(i w)     *'
-     write(*,*)'* and compute EcRPA and EcGM      *'
+     write(*,*)'* Use Xo(i w) to build X(i w),    *'
+     write(*,*)'*   compute EcRPA and EcGM,       *'
+     write(*,*)'*     and build Wp (i w)          *'
      write(*,*)'***********************************'
      write(*,*)
 
@@ -199,45 +202,48 @@ subroutine BQuAcK(working_dir,dotest,doHFB,doqsGW,nNuc,nBas,nOrb,nC,nO,nV,ENuc,Z
      EcRPA=0d0; EcGM=0d0;
      do ifreq=1,nfreqs
 
-      ! Initialization
-      trace=0d0; trace2=0d0; trace3=0d0; 
-      eigval_eps(:)=0d0
-      eigenv_eps(:,:)=0d0
-      eps(:,:)=0d0
-
-      ! Build Xo v
-      Chi0_ao_iw_v(:,:)=matmul(Real(Chi0_ao_iw(ifreq,:,:)),vMAT(:,:))
-
-      ! Tr [ Xo v ] and define eps
-      do ibas=1,nBas2
-       eps(ibas,ibas) = 1d0
-       trace=trace+Chi0_ao_iw_v(ibas,ibas)
-      enddo
-      eps(:,:) = eps(:,:) - Chi0_ao_iw_v(:,:)
-      
-      ! For GW 
-      ! a) build eps^-1
-      ! b) Set X v = eps^-1 Xo v 
-      call inverse_matrix(nBas2,eps,epsm1)
-      Chi_ao_iw_v(:,:)=matmul(epsm1(:,:),Real(Chi0_ao_iw(ifreq,:,:)))
-      Chi_ao_iw_v(:,:)=matmul(Chi_ao_iw_v(:,:),vMAT(:,:))
-
-      ! Tr [ X v ] for GM
-      do ibas=1,nBas2
-       trace3=trace3+Chi_ao_iw_v(ibas,ibas)
-      enddo
-
-      ! For RPA
-      call diagonalize_general_matrix(nBas2,eps,eigval_eps,eigenv_eps)
-      do ibas=1,nBas2
-       trace2=trace2+Log(abs(eigval_eps(ibas))) ! Recall that Tr [ U Log[Eig_Mat] U^T ] = Tr [ U^T U Log[Eig_Mat] ] =  Tr [ Log[Eig_Mat] ]
-                                                ! (i.e., we just need to sum the Log of the eigenvalues)
-      enddo
-      
-      ! Compute EcRPA and EcGM from traces 
-      EcRPA=EcRPA+wweight(ifreq)*(trace2+trace)/(2d0*pi)
-      EcGM =EcGM -wweight(ifreq)*(trace3-trace)/(2d0*pi)
-      
+       ! Initialization
+       trace=0d0; trace2=0d0; trace3=0d0; 
+       eigval_eps(:)=0d0
+       eigenv_eps(:,:)=0d0
+       eps(:,:)=0d0
+       
+       ! Build Xo v
+       Chi0_ao_iw_v(:,:)=matmul(Real(Chi0_ao_iw(ifreq,:,:)),vMAT(:,:))
+       
+       ! Tr [ Xo v ] and define eps
+       do ibas=1,nBas2
+        eps(ibas,ibas) = 1d0
+        trace=trace+Chi0_ao_iw_v(ibas,ibas)
+       enddo
+       eps(:,:) = eps(:,:) - Chi0_ao_iw_v(:,:)
+       
+       ! For GW 
+       ! a) build eps^-1
+       ! b) Set X v = eps^-1 Xo v 
+       call inverse_matrix(nBas2,eps,epsm1)
+       Chi_ao_iw_v(:,:)=matmul(epsm1(:,:),Real(Chi0_ao_iw(ifreq,:,:)))
+       Chi_ao_iw_v(:,:)=matmul(Chi_ao_iw_v(:,:),vMAT(:,:))
+       
+       ! Tr [ X v ] for GM
+       do ibas=1,nBas2
+        trace3=trace3+Chi_ao_iw_v(ibas,ibas)
+       enddo
+       
+       ! For RPA
+       call diagonalize_general_matrix(nBas2,eps,eigval_eps,eigenv_eps)
+       do ibas=1,nBas2
+        trace2=trace2+Log(abs(eigval_eps(ibas))) ! Recall that Tr [ U Log[Eig_Mat] U^T ] = Tr [ U^T U Log[Eig_Mat] ] =  Tr [ Log[Eig_Mat] ]
+                                                 ! (i.e., we just need to sum the Log of the eigenvalues)
+       enddo
+       
+       ! Compute EcRPA and EcGM from traces 
+       EcRPA=EcRPA+wweight(ifreq)*(trace2+trace)/(2d0*pi)
+       EcGM =EcGM -wweight(ifreq)*(trace3-trace)/(2d0*pi)
+       
+       ! Building Wp in AO basis (TODO: use it)
+       Wp_ao_iw(:,:)=matmul(vMAT(:,:),Chi_ao_iw_v(:,:))
+       
      enddo
 
      write(*,*)'-------------------------------------------------------------------------------'
@@ -248,7 +254,7 @@ subroutine BQuAcK(working_dir,dotest,doHFB,doqsGW,nNuc,nBas,nOrb,nC,nO,nV,ENuc,Z
 
      ! Deallocate arrays
      deallocate(eps,epsm1,eigval_eps,eigenv_eps)
-     deallocate(Chi0_ao_iw_v,Chi_ao_iw_v)
+     deallocate(Chi0_ao_iw_v,Chi_ao_iw_v,Wp_ao_iw)
 
      call wall_time(end_Xoiw)
      t_Xoiw = end_Xoiw - start_Xoiw
