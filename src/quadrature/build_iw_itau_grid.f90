7abc09b9a02a223d5c10dbc96bd43ae115f64a56
Mauricio Rodriguez-Mayorga <marm3.14@gmail.com>
2025-10-20 12:16:59 +0200

    Including better def and update gitignore

--- src/quadrature/build_iw_itau_grid.f90
+++ src/quadrature/build_iw_itau_grid.f90
@@ -49,8 +49,7 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
   double precision,allocatable   :: interval_tmp_weight(:,:),interval_tmp_coord(:,:)
   double precision,allocatable   :: interval_tmp_weight2(:,:),interval_tmp_coord2(:,:)
 
-  complex*16                     :: G_4vals(4)
-  complex*16,allocatable         :: G_test(:,:),G_tmp1(:,:)
+  complex*16                     :: G_2vals(2)
   complex*16,allocatable         :: interval_vals(:,:)
 
 ! Output variables
@@ -69,7 +68,6 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
  ngrid=40
  thrs_tnorm=1d-5
  min_teval=2d-4  ! Start tau grid at least at 2d-4 because when tau->0 G(i tau) is not continuos [ Lim tau->0+ /= Lim tau->0- ]
- allocate(G_test(nBas,nBas),G_tmp1(nbas,nBas))
  chem_pot = 0.5d0*(eHF(nO)+eHF(nO+1))
  eHF(:) = eHF(:)-chem_pot
  inquire(file='max_ntimes_nfreqs', exist=file_exists)
@@ -78,14 +76,6 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
   read(iunit,*) ntimes_max,nfreqs_max
   close(iunit)
  endif
-
- ! Set grid from 0 to 1
- n01 = 100 ! From 0 to 1 we always take 100 points
- kind_int = 1
- lim_inf = 0d0; lim_sup = 1d0;
- alpha = 0d0;  beta  = 0d0;
- allocate(weight_01(n01),coord_01(n01))
- call cgqf(n01,kind_int,alpha,beta,lim_inf,lim_sup,coord_01,weight_01)
 
  ! Use smallest exponent to find max_teval
  tstep=1d0
@@ -124,6 +114,17 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
  write(*,'(a, f20.10)') '  Maximum w value           : ',max_weval
  write(*,*)
 
+!-------------------------!
+! Prepare time Quadrature !
+!-------------------------!
+
+ ! Set grid from 0 to 1
+ n01 = 100 ! From 0 to 1 we always take 100 points
+ kind_int = 1
+ lim_inf = 0d0; lim_sup = 1d0;
+ alpha = 0d0;  beta  = 0d0;
+ allocate(weight_01(n01),coord_01(n01))
+ call cgqf(n01,kind_int,alpha,beta,lim_inf,lim_sup,coord_01,weight_01)
  ! Find tweight and tcoord with an adaptative quadrature
  iter=-1
  nintervals=1
@@ -168,15 +169,15 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
      ! For the ith active interval [a,b] compute all G_interval(iw_k) = -i int _a ^b [ G(i tau) Exp(-i tau w_k)  + G(-i tau) Exp(i tau w_k) ] d tau
      interval_vals=czero
      do ifreq=1,ngrid
-      G_4vals=czero
+      G_2vals=czero
       do itau=1,n01
        teval=interval_tmp_coord(iinterval,itau)
        tweight_eval=interval_tmp_weight(iinterval,itau)
-       G_4vals(1)=G_4vals(1)-im*tweight_eval*im*Exp(teval*eHF(nO))           *Exp(-im*teval*wcoord(ifreq)) !  Go(i tau)
-       G_4vals(2)=G_4vals(2)-im*tweight_eval*im*Exp(teval*(eHF(1)-eHF(nOrb)))*Exp(-im*teval*wcoord(ifreq)) ! ~Xo(i tau)
+       G_2vals(1)=G_2vals(1)-im*tweight_eval*im*Exp(teval*eHF(nO))           *Exp(-im*teval*wcoord(ifreq)) !  Go(i tau)
+       G_2vals(2)=G_2vals(2)-im*tweight_eval*im*Exp(teval*(eHF(1)-eHF(nOrb)))*Exp(-im*teval*wcoord(ifreq)) ! ~Xo(i tau)
       enddo
-      interval_vals(ifreq,1)=G_4vals(1)
-      interval_vals(ifreq,2)=G_4vals(2)
+      interval_vals(ifreq,1)=G_2vals(1)
+      interval_vals(ifreq,2)=G_2vals(2)
      enddo
      ! Divide the ith interval in halves and compute coords and weights
      interval_r2(2*iinterval-1,1)=interval_r(iinterval,1)
@@ -190,21 +191,21 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
      ! Check the difference in G_interval(iw_k) computed as [a,b] vs [a,0.5(b-a)+a] + [0.5(b-a)+a,b]
      err_G_set=0d0
      do ifreq=1,ngrid
-      G_4vals=czero
+      G_2vals=czero
       do itau=1,n01
        ! [a,0.5(b-a)+a]
        teval=interval_tmp_coord2(2*iinterval-1,itau)
        tweight_eval=interval_tmp_weight2(2*iinterval-1,itau)
-       G_4vals(1)=G_4vals(1)-im*tweight_eval*im*Exp(teval*eHF(nO))           *Exp(-im*teval*wcoord(ifreq)) !  Go(i tau)
-       G_4vals(2)=G_4vals(2)-im*tweight_eval*im*Exp(teval*(eHF(1)-eHF(nOrb)))*Exp(-im*teval*wcoord(ifreq)) ! ~Xo(i tau)
+       G_2vals(1)=G_2vals(1)-im*tweight_eval*im*Exp(teval*eHF(nO))           *Exp(-im*teval*wcoord(ifreq)) !  Go(i tau)
+       G_2vals(2)=G_2vals(2)-im*tweight_eval*im*Exp(teval*(eHF(1)-eHF(nOrb)))*Exp(-im*teval*wcoord(ifreq)) ! ~Xo(i tau)
        ! [0.5(b-a)+a,b]
        teval=interval_tmp_coord2(2*iinterval  ,itau)
        tweight_eval=interval_tmp_weight2(2*iinterval  ,itau)
-       G_4vals(1)=G_4vals(1)-im*tweight_eval*im*Exp(teval*eHF(nO))           *Exp(-im*teval*wcoord(ifreq)) !  Go(i tau)
-       G_4vals(2)=G_4vals(2)-im*tweight_eval*im*Exp(teval*(eHF(1)-eHF(nOrb)))*Exp(-im*teval*wcoord(ifreq)) ! ~Xo(i tau)
+       G_2vals(1)=G_2vals(1)-im*tweight_eval*im*Exp(teval*eHF(nO))           *Exp(-im*teval*wcoord(ifreq)) !  Go(i tau)
+       G_2vals(2)=G_2vals(2)-im*tweight_eval*im*Exp(teval*(eHF(1)-eHF(nOrb)))*Exp(-im*teval*wcoord(ifreq)) ! ~Xo(i tau)
       enddo
-      G_4vals(:)=G_4vals(:)-interval_vals(ifreq,:)
-      err_G_set=err_G_set+abs(G_4vals(1))+abs(G_4vals(2))
+      G_2vals(:)=G_2vals(:)-interval_vals(ifreq,:)
+      err_G_set=err_G_set+abs(G_2vals(1))+abs(G_2vals(2))
      enddo
      ! If we entered here, we always retain the new 2 halves of the original interval
      ! If diff. is small we keep the small intervals, but deactivate these two intervals
@@ -275,14 +276,14 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
   ! Check the global error for G(i tau) -> G(i w) and ~Xo(i tau) -> ~Xo(i w)
   err_G_set=0d0
   do ifreq=1,ngrid
-   G_4vals=czero
+   G_2vals=czero
    do itau=1,ntimes                        ! This is Go or ~Xo 
-    G_4vals(1)=G_4vals(1)-im*tweight(itau)*im*Exp(tcoord(itau)*eHF(nO))           *Exp(-im*tcoord(itau)*wcoord(ifreq)) !  Go(i tau)
-    G_4vals(2)=G_4vals(2)-im*tweight(itau)*im*Exp(tcoord(itau)*(eHF(1)-eHF(nOrb)))*Exp(-im*tcoord(itau)*wcoord(ifreq)) ! ~Xo(i tau)
+    G_2vals(1)=G_2vals(1)-im*tweight(itau)*im*Exp(tcoord(itau)*eHF(nO))           *Exp(-im*tcoord(itau)*wcoord(ifreq)) !  Go(i tau)
+    G_2vals(2)=G_2vals(2)-im*tweight(itau)*im*Exp(tcoord(itau)*(eHF(1)-eHF(nOrb)))*Exp(-im*tcoord(itau)*wcoord(ifreq)) ! ~Xo(i tau)
    enddo
-   G_4vals(1)=G_4vals(1)-1d0/(im*wcoord(ifreq)-eHF(nO))
-   G_4vals(2)=G_4vals(2)-1d0/(im*wcoord(ifreq)-(eHF(1)-eHF(nOrb)))
-   err_G_set=err_G_set+abs(G_4vals(1))+abs(G_4vals(2))
+   G_2vals(1)=G_2vals(1)-1d0/(im*wcoord(ifreq)-eHF(nO))
+   G_2vals(2)=G_2vals(2)-1d0/(im*wcoord(ifreq)-(eHF(1)-eHF(nOrb)))
+   err_G_set=err_G_set+abs(G_2vals(1))+abs(G_2vals(2))
   enddo
   err_G_set=err_G_set/(2*ngrid) ! MAE
   write(*,'(a,f8.4,a,i5,a,i8,a)') ' MAE in reproducing G(iw) and X(iw) [Integrated - Analytic]',err_G_set,' at iter ',iter,' with ',ntimes,' grid'
@@ -309,11 +310,6 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
   if(ntimes>ntimes_max) exit
  enddo
  write(*,*)
- deallocate(interval_vals)
- deallocate(interval_tmp_weight)
- deallocate(interval_tmp_coord)
- deallocate(interval_r,interval_todo)
-
  ! Print tweight and tcoord
  open(unit=iunit, form='formatted', file='tcoord.txt')
  do itau=1,ntimes
@@ -325,6 +321,12 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
   write(iunit,'(f50.15)') tweight(itau)
  enddo
  close(iunit)
+ ! Deallocate arrays
+ deallocate(interval_vals)
+ deallocate(interval_tmp_weight)
+ deallocate(interval_tmp_coord)
+ deallocate(interval_r,interval_todo)
+ deallocate(weight_01,coord_01)
  deallocate(tcoord,tweight)
  deallocate(wcoord)
 
@@ -359,9 +361,5 @@ subroutine build_iw_itau_grid(nBas,nOrb,nO,ntimes,nfreqs,verbose,cHF,eHF)
 
  ! Recover eHF initial values and deallocate arrays
  eHF(:) = eHF(:)+chem_pot
-
- ! Deallocate arrays 
- deallocate(weight_01,coord_01)
- deallocate(G_test,G_tmp1)
 
 end subroutine
