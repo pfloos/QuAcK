b6a8caa825fe41af85fa603d8bdd1afbd32f167e
Titou Loos <loos@irsamc.ups-tlse.fr>
2025-11-07 09:05:22 +0100

    fix stupidity

--- utils/update_input.sh
+++ utils/update_input.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
   echo "copying input/methods.default to input/methods" 
-  mv input/methods.default input/methods
+  cp input/methods.default input/methods
   echo "copying input/options.default to input/options" 
-  mv input/options.default input/options
+  cp input/options.default input/options
   echo "done!"
 
