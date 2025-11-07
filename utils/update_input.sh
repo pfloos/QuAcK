4b0691afa721c61e973699b1ceecfc8ee9cac55c
Titou Loos <loos@irsamc.ups-tlse.fr>
2025-11-07 09:04:02 +0100

    new script for updating input quantities

---
+++ utils/update_input.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+  echo "copying input/methods.default to input/methods" 
+  mv input/methods.default input/methods
+  echo "copying input/options.default to input/options" 
+  mv input/options.default input/options
+  echo "done!"
+
