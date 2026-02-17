63145f7c98b0df54cd4aa4b91a0edef3cc1702e1
lburth <lburth@irsamc.ups-tlse.fr>
2026-02-17 17:53:29 +0100

    fix rpax energy with exchange

--- src/RPA/CVS_phURPAx.f90
+++ src/RPA/CVS_phURPAx.f90
@@ -178,13 +178,6 @@ subroutine CVS_phURPAx(dotest,TDA,doACFDT,exchange_kernel,spin_conserved,spin_fl
   if(exchange_kernel) then
 
     EcRPA(1) = 0.5d0*EcRPA(1)
-    EcRPA(2) = 1.5d0*EcRPA(2)
-
-  end if
-
-  if(exchange_kernel) then
-
-    EcRPA(1) = 0.5d0*EcRPA(1)
     EcRPA(2) = 0.5d0*EcRPA(2)
 
   else
