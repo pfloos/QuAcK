9ac34699925b6b3f070ceddba968405cfb162657
lburth <loris.burth@irsamc.ups-tlse.fr>
2026-03-17 19:44:42 +0100

    Right vectors and screening in CVS phULR

--- src/LR/CVS_phULR.f90
+++ src/LR/CVS_phULR.f90
@@ -18,6 +18,7 @@ subroutine CVS_phULR(TDA,nSa,nSb,nSt,Aph,Bph,EcRPA,Om,XpY,XmY)
 
   double precision,external     :: trace_matrix
   double precision,allocatable  :: ApB(:,:)
+  double precision,allocatable  :: Aphtilde(:,:)
   double precision,allocatable  :: AmB(:,:)
   double precision              :: eF
   integer                       :: i
@@ -31,14 +32,10 @@ subroutine CVS_phULR(TDA,nSa,nSb,nSt,Aph,Bph,EcRPA,Om,XpY,XmY)
 
 ! Memory allocation
 
-  allocate(ApB(nSt,nSt),AmB(nSt,nSt))
+  allocate(ApB(nSt,nSt),AmB(nSt,nSt),Aphtilde(nSt,nSt))
 
 ! Tamm-Dancoff approximation
 
-!  eF       = 20d0
-!  do i=1,nSt
-!    Aph(i,i) = Aph(i,i) + 2*eF
-!  enddo
 
   if(TDA) then
 
@@ -48,44 +45,58 @@ subroutine CVS_phULR(TDA,nSa,nSb,nSt,Aph,Bph,EcRPA,Om,XpY,XmY)
     XmY(:,:) = XpY(:,:)
 
   else
+     
+    ! Build (\tilde{A} +B ) (\tilde{A} - B ) - 4*eF*\tilde{A} 
+    eF  = 0d0
+    Aphtilde = Aph
+    if(eF>0d0) then
+      do i=1,nSt
+        Aphtilde(i,i) = Aphtilde(i,i) + 2*eF
+      enddo
+    endif
 
-    ApB(:,:) = Aph(:,:) + Bph(:,:) 
-    AmB(:,:) = Aph(:,:) - Bph(:,:)
+    ApB(:,:) = Aphtilde(:,:) + Bph(:,:) 
+    AmB(:,:) = Aphtilde(:,:) - Bph(:,:)
 
-
-  ! Diagonalize linear response matrix
-   
-!    ApB = matmul(ApB,AmB)
-!    ApB = ApB - 4*eF*Aph
-!    call diagonalize_general_matrix(nSt,ApB,Om,AmB)
-!    call vecout(nSt,sqrt(Om + 4*eF**2))
     ApB = matmul(ApB,AmB)
+    ApB(:,:) = ApB(:,:) - 4*eF*Aphtilde(:,:)
+
+    ! Diagonalize linear response matrix
     call diagonalize_general_matrix(nSt,ApB,Om,XmY)
-    Om = sqrt(Om)
-    XpY = matmul(AmB,XmY)
-    XpY = transpose(XpY)
-    call DA(nSt,1d0/Om,XpY)
-    XpY = transpose(XpY)
+    Om = sqrt(Om + 4*eF**2)
     
+    ! Get XpY via (X+Y) =(\tilde{A}-B - 2*eF*Id)(X-Y)Omega^-1
+    XpY = matmul(AmB,XmY) - 2*eF*XmY
+    call AD(nSt,XpY,1/Om)
+     
+    ! Compute Overlap and assign ex-/deexcitations
     ApB = matmul(transpose(XmY),XpY)
     do i=1,nSt
       if(ApB(i,i)<0d0) then
         Om(i)    = - Om(i)
-        XmY(:,i) = - XmY(:,i)
+        ! Correct column for right sign of Om 
+        XpY(:,i) = - XpY(:,i) 
       end if
     enddo
+    
     call sort_eigenvalues_vec_vec(nSt,Om,XmY,XpY)
+    
+    ! Orthonormalize
     ApB = matmul(transpose(XmY),XpY)
     call orthogonalize_matrix(1,nSt,ApB,AmB)
     XmY = matmul(XmY,AmB)
     XpY = matmul(XpY,AmB)
+    
+    ! Transpose XmY and XpY because Quack wants this
     XmY = transpose(XmY) 
-    XpY = transpose(XpY) 
+    XpY = transpose(XpY)
     
   end if
 
 ! Compute the RPA correlation energy
 
   EcRPA = 0.5d0*(sum(Om) - trace_matrix(nSt,Aph))
+  
+  deallocate(ApB,AmB,Aphtilde)
 
 end subroutine 
