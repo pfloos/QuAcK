eecf1dd7eeb8ebb98c9f66634b786b7e6185fd02
Antoine Marie <amarie@Antoines-MacBook-Pro.local>
2026-03-27 15:22:24 +0100

    CHANGE METHODS, introducing adc way of computing GF2, going toward psd GF3

---
+++ src/GF/RpsdG0F3.f90
@@ -0,0 +1,109 @@
+subroutine RpsdG0F3(dotest,linearize,eta,doSRG,nBas,nOrb,nC,nO,nV,nR,nS,ENuc,ERHF,ERI,eHF)
+
+! Perform a one-shot third-order Green function calculation
+
+  implicit none
+  include 'parameters.h'
+
+! Input variables
+
+  logical,intent(in)            :: dotest
+
+  logical,intent(in)            :: linearize
+  double precision,intent(in)   :: eta
+  logical,intent(in)            :: doSRG
+
+  integer,intent(in)            :: nBas
+  integer,intent(in)            :: nOrb
+  integer,intent(in)            :: nC
+  integer,intent(in)            :: nO
+  integer,intent(in)            :: nV
+  integer,intent(in)            :: nR
+  integer,intent(in)            :: nS
+  double precision,intent(in)   :: ENuc
+  double precision,intent(in)   :: ERHF
+  double precision,intent(in)   :: ERI(nOrb,nOrb,nOrb,nOrb)
+  double precision,intent(in)   :: eHF(nOrb)
+
+! Local variables
+
+  double precision              :: Ec
+  double precision              :: flow
+  double precision,allocatable  :: eGFlin(:)
+  double precision,allocatable  :: eGF(:)
+  double precision,allocatable  :: SigC(:)
+  double precision,allocatable  :: Z(:)
+
+! Hello world
+
+  write(*,*)
+  write(*,*)'*******************************'
+  write(*,*)'* Restricted G0F3 Calculation *'
+  write(*,*)'*******************************'
+  write(*,*)
+
+! SRG regularization
+
+  flow = 500d0
+
+  if(doSRG) then
+
+    write(*,*) '*** SRG regularized G0F3 scheme ***'
+    write(*,*)
+
+  end if
+  
+! Memory allocation
+
+  allocate(SigC(nOrb), Z(nOrb), eGFlin(nOrb), eGF(nOrb))
+  SigC(:) = 0d0
+  Z(:) = 0d0
+  eGFlin(:) = 0d0
+  eGF(:) = 0d0
+
+! Frequency-dependent third-order contribution
+
+  if(doSRG) then 
+
+    ! TODO call RGF3_SRG_self_energy_diag(flow,nOrb,nC,nO,nV,nR,eHF,ERI,Ec,SigC,Z)
+
+  else
+
+    call RpsdGF3_self_energy_diag(eta,nOrb,nC,nO,nV,nR,eHF,ERI,SigC,Z)
+
+  end if
+  
+  eGFlin(:) = eHF(:) + Z(:)*SigC(:)
+
+  if(linearize) then
+
+    write(*,*) '*** Quasiparticle energies obtained by linearization ***'
+
+    eGF(:) = eGFlin(:)
+
+  else
+
+    write(*,*) ' *** Quasiparticle energies obtained by root search *** '
+    write(*,*)
+
+    call RGF3_QP_graph(doSRG,eta,flow,nOrb,nC,nO,nV,nR,eHF,ERI,eGFlin,eHF,eGF,Z)
+
+  end if
+
+  ! Print results
+
+  call print_RG0F3(nOrb,nC,nO,nV,nR,eHF,SigC,eGF,Z,ENuc,ERHF,Ec)
+
+! Testing zone
+
+  if(dotest) then
+
+    call dump_test_value('R','G0F2 correlation energy',Ec)
+    call dump_test_value('R','G0F2 HOMO energy',eGF(nO))
+    call dump_test_value('R','G0F2 LUMO energy',eGF(nO+1))
+
+  end if
+
+  deallocate(SigC, Z, eGFlin, eGF)
+  
+end subroutine RPSDG0F3
