659c5866c78f650153d4917ce24396d9ec2028f5
lburth <loris.burth@irsamc.ups-tlse.fr>
2026-04-01 11:28:06 +0200

    small adapt in print of excitation energies

--- src/RPA/complex_phRRPAx.f90
+++ src/RPA/complex_phRRPAx.f90
@@ -123,7 +123,7 @@ subroutine complex_phRRPAx(dotest,TDA,doACFDT,exchange_kernel,singlet,triplet,nB
     if(.not.TDA) call complex_CVS_phRLR_B(ispin,dRPA,nBas,nC,nO,nV,nR,nSCVS,nCVS,nFC,occupations_fc,virtuals,lambda,ERI,Bph)
 
     call complex_phRLR(TDA,nSCVS,Aph,Bph,EcRPA(ispin),Om,XpY,XmY)
-    call complex_print_excitation_energies('phRPAx@RHF','singlet',nS,Om)
+    call complex_print_excitation_energies('phRPAx@RHF','singlet',nSCVS,Om)
     call complex_phLR_transition_vectors(ispin,nBas,nC,nO,nV,nR,nS,&
                 nCVS,nFC,occupations_fc,virtuals,Om,XpY,XmY)
 
