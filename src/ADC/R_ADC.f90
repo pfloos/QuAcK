51faf95824149f129b4a0644cfc07471f5bc1782
pfloos <pierrefrancois.loos@gmail.com>
2026-05-07 22:58:15 +0200

    saving work

--- src/ADC/R_ADC.f90
+++ src/ADC/R_ADC.f90
@@ -214,7 +214,7 @@ subroutine R_ADC(dotest,                                               &
     if(do_SOSEX) then 
       
       call wall_time(start_ADC)
-      call R_SOSEX(dotest,TDA_W,singlet,triplet,linearize,eta,doSRG,nBas,nOrb,nC,nO,nV,nR,nS,ENuc,ERHF,ERI_MO,dipole_int_MO,eHF)
+      call R_SOSEX(dotest,TDA_W,singlet,triplet,linearize,eta,doSRG,flow,nBas,nOrb,nC,nO,nV,nR,nS,ENuc,ERHF,ERI_MO,dipole_int_MO,eHF)
       call wall_time(end_ADC)
     
       t_ADC = end_ADC - start_ADC
